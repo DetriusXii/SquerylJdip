09de4530755500a6761e54e10f26c666acd72929
Andrijan Jelovic <ajelovic@yahoo.com>
2011-08-30 07:28:17 -0600

    Main.scala:  Removing the unnecessary imports from Main.scala Jdip.scala:  Adding the imports to Jdip.scala

--- src/com/squeryl/jdip/Main.scala
+++ src/com/squeryl/jdip/Main.scala
@@ -13,69 +13,8 @@ import java.sql.{Array => _, _}
 import org.squeryl.dsl.CompositeKey3
 import org.squeryl.dsl.CompositeKey4
 import org.squeryl.dsl.ast._
-import org.squeryl.internals.FieldMetaData
-
-object Jdip extends Schema {
-   val games = table[Games]("games", "jdip")
-   val players = table[Players]("players", "jdip")
-   val gamePlayerRelations = 
-     manyToManyRelation(games, players, "gamePlayerRelations", "jdip").via[GamePlayerRelations](
-       (g,p,gp) => {
-         (g.id === gp.gameName, p.id === gp.playerName)
-       }
-     )
-   val countries = table[Countries]("countries", "jdip")
-   val unitTypes = table[UnitTypes]("unitTypes", "jdip")
-   val gameState = table[GameState]("gameState", "jdip")
-   
-
-   val orderTypes = table[OrderTypes]("orderTypes", "jdip")
-   val orders = table[Orders]("orders", "jdip")
-   
-   val gamePlayerCountryRelations = 
-     manyToManyRelation(gamePlayerRelations, countries, "gamePlayerCountryRelations", "jdip").
-      via[GamePlayerCountryRelations]((gpr, c, gpcr) => 
-        (gpr.id === gpcr.gamePlayerRelationsId, c.id === gpcr.countryName)
-      )
-  val phase = table[Phase]("phase", "jdip")
-  val season = table[Season]("season", "jdip")
-      
-  val messages = table[Messages]("messages", "jdip")
-  val senderMessageForeignKey = 
-    oneToManyRelation(gamePlayerCountryRelations, messages).via((gpcr, m) =>
-      gpcr.id === m.senderId
-    )
-  val receiverMessageForeignKey =
-    oneToManyRelation(gamePlayerCountryRelations, messages).via((gpcr, m) =>
-      gpcr.id === m.receiverId
-    )
- 
-  val gameNameGameStateForeignKey =
-    oneToManyRelation(games, gameState).via((g, gs) => {
-        g.id === gs.gameName
-    })
-  val seasonForeignKey = oneToManyRelation(season, gameState).via((s, gs) =>
-    s.id === gs.season
-  })
-  val phaseForeignKey = oneToManyRelation(phase, gameState).via((p, gs) => {
-        p.id === gs.phase
-    })
-
-  val gameStateOrdersForeignKey = oneToManyRelation(gameState, orders).via((gs, o) => {
-        gs.id === o.gameStateId
-    })
-  val gamePlayerCountryRelationsOrdersForeignKey = 
-    oneToManyRelation(gamePlayerCountryRelations, orders).via((gpcr, o) => {
-        gpcr.id === o.gamePlayerCountryRelationsId
-    })
-  val orderTypeOrdersForeignKey = oneToManyRelation(orderType, orders).via((ot, o) => {
-        ot.id === o.orderType
-    })
-  val unitTypesOrdersForeignKey = oneToManyRelation(unitType, orders).via((ut, o) => {
-        ut.id === o.unitType
-    })
-
-}
+import com.squeryl.jdip.tables._
+import com.squeryl.jdip.schemas.Jdip
 
 object Main {
 
