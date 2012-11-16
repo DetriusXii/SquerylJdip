a37e456638d1536ea9aacf3d22eebacff6242c4f
Andrijan Jelovic <ajelovic@yahoo.com>
2012-11-16 08:16:09 -0600

    Attached the presentation name into Location.scala to avoid the repetition of calling DiplomacyQueries.getFormattedLocationName

--- src/com/squeryl/jdip/tables/Location.scala
+++ src/com/squeryl/jdip/tables/Location.scala
@@ -4,6 +4,10 @@ import org.squeryl.KeyedEntity
 
 case class Location(province: String, coast: String) extends KeyedEntity[Int] {
 	val id = 0
+	val presentationName = coast match {
+	  case Coast.ALL_COAST | Coast.NO_COAST => province
+	  case _ => "%s-%s" format (province, coast)
+	}
 	
 	def this() = this("", "")
 }
