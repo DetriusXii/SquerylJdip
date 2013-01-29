0ecce9cc755125a50e92b4989cc1143e4f909199
Andrijan Jelovic <ajelovic@yahoo.com>
2013-01-29 07:15:20 -0600

    Removed the validation from InsertStatements and UpdateStatements

--- src/com/squeryl/jdip/queries/InsertStatements.scala
+++ src/com/squeryl/jdip/queries/InsertStatements.scala
@@ -4,38 +4,16 @@ import com.squeryl.jdip.tables._
 import org.squeryl.PrimitiveTypeMode._
 import com.squeryl.jdip.schemas.Jdip
 import java.util.Date
+import java.sql.Timestamp
 
 object InsertStatements {
 	def insertMovementPhaseOrder(dpu: DiplomacyUnit, orderType: String, 
 	    simpleOrSrcLocationOption: Option[Location], 
-	    advancedDstLocationOption: Option[Location]): Option[Unit] = transaction {
-	  orderType match {
-	    case OrderType.HOLD => Some(Jdip.orders.insert(Order(dpu.id, 
-	        orderType, 
-	        new java.sql.Timestamp(new Date().getTime), 
-	        None, 
-	        None)))
-	    case OrderType.MOVE | OrderType.SUPPORT_HOLD => 
-	      if (simpleOrSrcLocationOption.isDefined && advancedDstLocationOption.isEmpty) {
-	        Some(Jdip.orders.insert(Order(dpu.id, 
-	            orderType, 
-	            new java.sql.Timestamp(new Date().getTime),
-	            simpleOrSrcLocationOption.map(_.id),
-	            None)))
-	      } else {
-	        None
-	      }
-	    case OrderType.SUPPORT_MOVE | OrderType.CONVOY => 
-	      if (simpleOrSrcLocationOption.isDefined && advancedDstLocationOption.isDefined) {
-	    	  Some(Jdip.orders.insert(
-	    			  Order(dpu.id,  
-	    					  orderType, 
-	    					  new java.sql.Timestamp(new Date().getTime), 
-	    					  simpleOrSrcLocationOption.map(_.id),
-	    					  advancedDstLocationOption.map(_.id))))
-	      } else {
-	        None
-	      }
-	  }
+	    advancedDstLocationOption: Option[Location]): Unit = transaction {
+	  Jdip.orders.insert(
+	      Order(dpu.id, orderType, new Timestamp(new Date().getTime()), 
+	    		  simpleOrSrcLocationOption.map(_.id),
+	    		  advancedDstLocationOption.map(_.id)
+	      ))
 	}
 }
